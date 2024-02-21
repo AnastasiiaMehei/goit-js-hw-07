b9c52fd49f704155c8f5cd7aaae3c8a0e5fe1bb8
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-02-21 20:57:57 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,8 @@
-// const categories = document.querySelectorAll(".categories");
-// // categories.childrenNodes;
-// console.log(categories);
+const elements = document.querySelectorAll(".item");
+const numberElement = elements.length;
+const text = document.querySelectorAll("h2");
+console.log(text.innerHTML);
+const textContent = elements.textContent;
+console.log(`Number of categories: ${numberElement}`);
+console.log(textContent);
+console.log(`Category: ${textContent}`);
