60d917fdc8ac871a56aa6a73f831dd816e3d2301
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-02-21 22:38:44 +0200

    3

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,9 @@
 const elements = document.querySelectorAll(".item");
+console.log(elements);
 const numberElement = elements.length;
-const text = document.querySelectorAll("h2");
-console.log(text.innerHTML);
-const textContent = elements.textContent;
 console.log(`Number of categories: ${numberElement}`);
-console.log(textContent);
-console.log(`Category: ${textContent}`);
+
+const elLi = elements.forEach((element) => {
+  console.log(`Category: ${element.firstElementChild.textContent}`);
+  console.log(`Elements: ${element.querySelectorAll("li").length}`);
+});
--- js/task-2.js
+++ js/task-2.js
@@ -24,17 +24,12 @@ const images = [
     alt: "Zebras on Zebra",
   },
 ];
-const images = [
-  {
-    url: "<https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260>",
-    alt: "White and Black Long Fur Cat",
-  },
-  {
-    url: "<https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260>",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-  },
-  {
-    url: "<https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260>",
-    alt: "Group of Horses Running",
-  },
-];
+
+// const elements = document.querySelector("img-styles");
+// const imageEl = document.createElement("img");
+// imageEl.url = "";
+const element = images.map(
+  (img) => `<li><img src="${img.url}" alt="${img.alt}"/><li/>`
+);
+const galleryList = document.querySelector(".gallery");
+galleryList.insertAdjacentHTML("beforeend", element);
